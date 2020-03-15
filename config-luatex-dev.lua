f4bd0248896438c50c25d6059f95f3f2bbc742a5
Ulrike Fischer <fischer@troubleshooting-tex.de>
2020-03-15 23:56:02 +0100

    adapt config

--- config-luatex-dev.lua
+++ config-luatex-dev.lua
@@ -1,6 +1,6 @@
 testfiledir  = "testfiles-luatex-dev"
-checkengines = {"luahbtex"}
-stdengine    = "luahbtex"
+-- checkengines = {"luahbtex"}
+-- stdengine    = "luahbtex"
 checkruns    = 3
 checkformat  ="latex-dev"
 checksearch=true
