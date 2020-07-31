b4eb6f1a16f843024e75a52d698810bc3b94ecb8
Ulrike Fischer <fischer@troubleshooting-tex.de>
2020-07-31 17:22:01 +0200

    adapt tests to latex-dev and pdfresources changes

--- resave.bat
+++ resave.bat
@@ -1,3 +1,4 @@
-
-
-  l3build save -eluatex -cconfig-luatex test-patch-koma
+REM  l3build save -epdftex test-links-backend
+REM  l3build save -epdftex test-links-backend
+REM l3build save -cconfig-luatex test-luatex
+REM l3build save -epdftex -cconfig-pdftex test-attribute
