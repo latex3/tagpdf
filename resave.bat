245b46f29822757747352ba409b66585fbf95a20
Ulrike Fischer <fischer@troubleshooting-tex.de>
2020-08-04 19:18:33 +0200

    remade tests after format changes

--- resave.bat
+++
@@ -1,4 +0,0 @@
-REM  l3build save -epdftex test-links-backend
-REM  l3build save -epdftex test-links-backend
-REM l3build save -cconfig-luatex test-luatex
-REM l3build save -epdftex -cconfig-pdftex test-attribute
