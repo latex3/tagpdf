4ae739bf87def566c3f1bcd5657a6b3b8c8176d6
Ulrike Fischer <fischer@troubleshooting-tex.de>
2020-09-10 20:24:35 +0200

    remade test after lua name change and hss in shipout

--- resave.bat
+++ resave.bat
@@ -1,2 +1,14 @@
 rem */\(1+[a-z\-]\)\.\(2+[a-z]\)\.*
 rem l3build save -e\2 \1
+
+
+
+l3build save -epdftex -cconfig-pdftex test-patch-koma
+
+l3build save -eluatex -cconfig-nosyntax test-mc-low-bdc-emc
+l3build save -epdftex -cconfig-nosyntax  test-mc-low-bdc-emc
+l3build save -eluatex -cconfig-nosyntax  test-mcid-cnt
+l3build save -epdftex -cconfig-nosyntax  test-mcid-cnt
+
+l3build save -eluatex -cconfig-luatex test-luatex
+l3build save -eluatex -cconfig-luatex test-patch-koma
