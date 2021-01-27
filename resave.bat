657023ed601afb01aa8ceb05c03ba422d2e93601
Ulrike Fischer <fischer@troubleshooting-tex.de>
2021-01-27 17:53:19 +0100

    remade tests (opacity) and correct one (missing ifluatex))

--- resave.bat
+++ resave.bat
@@ -1,2 +1,32 @@
 rem */\(1+[a-z\-]\)\.\(2+[a-z]\)\.*
 rem l3build save -e\2 \1
+
+
+
+REM Failed tests for configuration config-pdftex:
+REM
+REM   Check failed with difference files
+REM   - ./build/test-config-pdftex/test-attribute.pdftex.pdf.fc
+REM   - ./build/test-config-pdftex/test-fakespace-tagpdf.pdftex.pdf.fc
+REM   - ./build/test-config-pdftex/test-mc-alt.pdftex.pdf.fc
+REM   - ./build/test-config-pdftex/test-pdf.pdftex.pdf.fc
+REM   - ./build/test-config-pdftex/test-sectioning-koma.pdftex.pdf.fc
+REM
+REM Failed tests for configuration config-nosyntax:
+REM
+REM   Check failed with difference files
+REM   - ./build/test-config-nosyntax/test-mc-low-bdc-emc.luatex.fc
+REM   - ./build/test-config-nosyntax/test-mcid-cnt.luatex.fc
+REM
+REM Failed tests for configuration config-luatex:
+REM
+REM   Check failed with difference files
+REM   - ./build/test-config-luatex/issue13-mcid-array.luatex.pdf.fc
+REM   - ./build/test-config-luatex/issue14-artifact-bool.luatex.pdf.fc
+REM   - ./build/test-config-luatex/test-attribute.luatex.pdf.fc
+REM   - ./build/test-config-luatex/test-fakespace-fontchange.luatex.pdf.fc
+REM   - ./build/test-config-luatex/test-fakespace-tagpdf.luatex.pdf.fc
+REM   - ./build/test-config-luatex/test-luatex-pdf.luatex.pdf.fc
+REM   - ./build/test-config-luatex/test-luatex.luatex.fc
+REM   - ./build/test-config-luatex/test-mc-alt.luatex.pdf.fc
+REM   - ./build/test-config-luatex/test-sectioning-koma.luatex.pdf.fc
