e8cf76ebed3f6be5fc97df49bd9cc896c26456f8
Ulrike Fischer <fischer@troubleshooting-tex.de>
2021-01-27 18:47:01 +0100

    last bunch of testfiles

--- resave.bat
+++ resave.bat
@@ -1,19 +1,2 @@
-rem */\(1+[a-z\-]\)\.\(2+[a-z]\)\.*
+rem */\(1+[a-z0-9\-]\)\.\(2+[a-z]\)\.*
 rem l3build save -e\2 \1
-
-REM   Check failed with difference files
-REM   - ./build/test-config-nosyntax/test-mc-low-bdc-emc.luatex.fc
-REM   - ./build/test-config-nosyntax/test-mcid-cnt.luatex.fc
-REM
-REM Failed tests for configuration config-luatex:
-REM
-REM   Check failed with difference files
-REM   - ./build/test-config-luatex/issue13-mcid-array.luatex.pdf.fc
-REM   - ./build/test-config-luatex/issue14-artifact-bool.luatex.pdf.fc
-REM   - ./build/test-config-luatex/test-attribute.luatex.pdf.fc
-REM   - ./build/test-config-luatex/test-fakespace-fontchange.luatex.pdf.fc
-REM   - ./build/test-config-luatex/test-fakespace-tagpdf.luatex.pdf.fc
-REM   - ./build/test-config-luatex/test-luatex-pdf.luatex.pdf.fc
-REM   - ./build/test-config-luatex/test-luatex.luatex.fc
-REM   - ./build/test-config-luatex/test-mc-alt.luatex.pdf.fc
-REM   - ./build/test-config-luatex/test-sectioning-koma.luatex.pdf.fc
