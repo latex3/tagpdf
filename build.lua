523ed9643c75154ee473306ef85a672d08c851d7
Ulrike Fischer <fischer@troubleshooting-tex.de>
2021-02-20 00:36:59 +0100

    adapt to rename in pdfresources, tests must be still checked

--- build.lua
+++ build.lua
@@ -51,6 +51,9 @@ checkruns = 3
 checksuppfiles = {"pdfaPilotSyntaxChecks.kfpx","checksyntax.bat","checksyntax-all.bat"}
 excludetests = {"test-saveboxes-structure-dev"}
 
+sourcefiles =  {"*.dtx","*.ins",
+                "tagpdf-regressiontest.sty"
+               } 
 
 if os.getenv('TRAVIS')  then
    checkconfigs = {"build","config-pdftex","config-luatex"}
