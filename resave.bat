528ff6f97a76d7ec8b2042b6728848e3d5fd1df0
Ulrike Fischer <fischer@troubleshooting-tex.de>
2021-02-21 01:21:07 +0100

    resave tests

--- resave.bat
+++ resave.bat
@@ -3,26 +3,15 @@ rem l3build save -e\2 \1
 rem l3build save -cconfig-pdftex -e\2 \1
 rem l3build save -cconfig-luatex -e\2 \1
 
-REMCheck failed with difference files
-REM  - ./build/test-config-luatex/issue13-mcid-array.luatex.pdf.fc
-REM  - ./build/test-config-luatex/test-patch-koma.luatex.fc
-REM
-REMFailed tests for configuration build:
-REM
 REM  Check failed with difference files
-REM  - ./build/test/test-getmctag.luatex.fc
-REM  - ./build/test/test-mc-ifinartifact.luatex.fc
-REM  - ./build/test/test-mc-low-luaonly.luatex.fc
-REM  - ./build/test/test-mc-tag-expansion.luatex.fc
-REM  - ./build/test/test-showattributes.luatex.fc
-REM
-REMFailed tests for configuration config-pdftex:
-REM
-REM  Check failed with difference files
-REM  - ./build/test-config-pdftex/test-attribute.pdftex.pdf.fc
+l3build save -eluatex test-getmctag
+l3build save -eluatex test-mc-ifinartifact
+l3build save -eluatex test-mc-low-luaonly
+l3build save -eluatex test-mc-tag-expansion
+l3build save -eluatex test-showattributes
 REM
 REMFailed tests for configuration config-luatex:
 REM
 REM  Check failed with difference files
-REM  - ./build/test-config-luatex/issue13-mcid-array.luatex.pdf.fc
-REM  - ./build/test-config-luatex/test-patch-koma.luatex.fc
+l3build save -cconfig-luatex -eluatex issue13-mcid-array
+l3build save -cconfig-luatex -eluatex test-patch-koma
