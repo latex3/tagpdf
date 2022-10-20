6763ac3e2fed8dae4b8b314f49761be50a2cdc28
Ulrike Fischer <fischer@troubleshooting-tex.de>
2022-10-20 20:48:31 +0200

    update test files

--- resave.bat
+++ resave.bat
@@ -4,15 +4,6 @@ rem l3build save -cconfig-pdftex -e\2 \1
 rem l3build save -cconfig-luatex -e\2 \1
 
 
-REM  Check failed with difference files
-l3build save -cconfig-luatex -eluatex issue13-mcid-array
-l3build save -cconfig-luatex -eluatex issue14-artifact-bool
-l3build save -cconfig-luatex -eluatex test-attribute
-l3build save -cconfig-luatex -eluatex test-fakespace-box
-l3build save -cconfig-luatex -eluatex test-fakespace-fontchange
-l3build save -cconfig-luatex -eluatex test-fakespace-tagpdf
-l3build save -cconfig-luatex -eluatex test-luatex-pdf
-l3build save -cconfig-luatex -eluatex test-luatex
-l3build save -cconfig-luatex -eluatex test-mc-alt
-l3build save -cconfig-luatex -eluatex test-patch-koma
-l3build save -cconfig-luatex -eluatex test-sectioning-koma
+
+l3build save -cconfig-pdftex -epdftex test-beast-1
+l3build save -cconfig-pdftex -epdftex test-namespace-pdf-article
