8dcd62e847335f458e179766986d014ab9dab4a8
Ulrike Fischer <fischer@troubleshooting-tex.de>
2022-11-11 00:28:07 +0100

    update tests

--- resave.bat
+++ resave.bat
@@ -3,26 +3,16 @@ rem l3build save -e\2 \1
 rem l3build save -cconfig-pdftex -e\2 \1
 rem l3build save -cconfig-luatex -e\2 \1
 
-l3build save -cconfig-pdftex -epdftex ptagging-004-p
-l3build save -cconfig-pdftex -epdftex ptagging-006-p
-l3build save -cconfig-pdftex -epdftex ptagging-007-p
-l3build save -cconfig-pdftex -epdftex test-attribute-name
-l3build save -cconfig-pdftex -epdftex test-attribute
-l3build save -cconfig-pdftex -epdftex test-fakespace-tagpdf
-l3build save -cconfig-pdftex -epdftex test-mc-alt
-l3build save -cconfig-pdftex -epdftex test-namespace-pdf-article
-l3build save -cconfig-pdftex -epdftex test-namespace-pdf-book
-l3build save -cconfig-pdftex -epdftex test-sectioning-koma
 
 REM 
 REM Failed tests for configuration config-luatex:
 REM 
 REM   Check failed with difference files
-REM   - ./build/test-config-luatex/issue13-mcid-array.luatex.fc
-REM   - ./build/test-config-luatex/issue14-artifact-bool.luatex.fc
-REM   - ./build/test-config-luatex/test-attribute.luatex.fc
-REM   - ./build/test-config-luatex/test-fakespace-box.luatex.fc
-REM   - ./build/test-config-luatex/test-fakespace-fontchange.luatex.fc
-REM   - ./build/test-config-luatex/test-fakespace-tagpdf.luatex.fc
-REM   - ./build/test-config-luatex/test-mc-alt.luatex.fc
-REM   - ./build/test-config-luatex/test-sectioning-koma.luatex.fc
+l3build save -cconfig-luatex -eluatex issue13-mcid-array
+l3build save -cconfig-luatex -eluatex issue14-artifact-bool
+l3build save -cconfig-luatex -eluatex test-attribute
+l3build save -cconfig-luatex -eluatex test-fakespace-box
+l3build save -cconfig-luatex -eluatex test-fakespace-fontchange
+l3build save -cconfig-luatex -eluatex test-fakespace-tagpdf
+l3build save -cconfig-luatex -eluatex test-mc-alt
+l3build save -cconfig-luatex -eluatex test-sectioning-koma
