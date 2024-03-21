9d0911c3bda20a767cfa22483c3c58076c8ca833
Ulrike Fischer <fischer@troubleshooting-tex.de>
2024-03-21 19:01:44 +0100

    fix recording of used attributes

--- resave.bat
+++ resave.bat
@@ -8,12 +8,6 @@ REM
 REM Failed tests for configuration config-luatex:
 REM 
 REM   Check failed with difference files
-  l3build save test-add-new-tag-2
-  l3build save -c config-pdftex test-struct-OBJR test-root-AF test-ref test-AFref show-variables-17 test-beast-1 test-namespace-pdfII-book test-namespace-pdfII-article test-link test-AF-file test-header-footer test-beast-2 parent-child-20-structures show-variables-20
-  l3build save -c config-luatex parent-child-20-structures test-header-footer test-struct-OBJR test-root-AF test-ref test-link test-AF-file
 
-
-
-  l3build check --show-saves test-add-new-tag-2
-  l3build check --show-saves -c config-pdftex test-struct-OBJR test-root-AF test-ref test-AFref show-variables-17 test-beast-1 test-namespace-pdfII-book test-namespace-pdfII-article test-link test-AF-file test-header-footer test-beast-2 parent-child-20-structures show-variables-20
-  l3build check --show-saves -c config-luatex parent-child-20-structures test-header-footer test-struct-OBJR test-root-AF test-ref test-link test-AF-file
+l3build check -S openaction-2 test-root-AF test-attribute-dev test-reset-attributes test-attribute-name test-attribute openaction-1 openaction-0
+REM  l3build save -c config-luatex test-fakespace-gh72 test-reset-attributes test-attribute test-root-AF
