ccf63dd8f658fb93680044711bc63e72cada76b4
Ulrike Fischer <fischer@troubleshooting-tex.de>
2024-04-11 16:12:04 +0200

    store test result

--- resave.bat
+++ resave.bat
@@ -9,5 +9,10 @@ REM Failed tests for configuration config-luatex:
 REM 
 REM   Check failed with difference files
 
- l3build save teststructref test-mc-ifinartifact test-links-backend test-fancyhdr test-paratag test-box-is-tagged test-mc-used-twice test-lang-change test-mc-used-twice-b open-structures texsource test-parent-key test-empty-mc mc-parent-child test-get-counter test-get-counter-base remap-tags test-local-inactive-mc test-add-mathml test-artifact-group test-getmctag
-  l3build save -c config-pdftex testauxtree test-header-footer test-mc-low-luaonly test-struct-user test-namespace-pdf-book test-struct-OBJR test-showattributes test-sectioning-koma test-saveboxes-structure debug-structures-2 test-root-AF test-beast-2 ptagging-005 parent-child-rolemap-17 ptagging-002 test-mc-alt test-patch-koma check-child test-ref ptagging-004-p test-mc-manual-para-split test-fakespace-tagpdf test-AFref openaction-2 test-reset-attributes ptagging-006 test-parentkey para-main-struct test-namespace-pdfII-book test-namespace-pdfII-article test-attribute-dev test-alttext test-namespace-pdf-article test-beast-1 test-mcid-cnt test-mc-tag-expansion openaction-1 test-link orphan-01 parent-child-17 parent-child-rolemap-20 test-mc-low-bdc-emc parent-child-17-nonstruct parent-child-20 test-mc-user ptagging-004 test-header-tagging orphan-02 ptagging-003 ptagging-006-p test-attribute test-AF-file openaction-0 socket-2 ptagging-007-p test-attribute-name parent-child-20-structures debug-structures test-mc-savebox parent-child-17-structures disable-delayed-shipout parent-child-20-nonstruct ptagging-001b
+REM object tracking 2024-04-11
+l3build save spacecharoff
+  l3build save -e pdftexmain remap-tags
+  l3build save -c config-pdftex -e pdftexmain test-AF-file ptagging-007-p test-mc-alt test-sectioning-koma openaction-0 openaction-2 ptagging-004-p test-struct-OBJR test-saveboxes-structure ptagging-006-p test-beast-2 test-reset-attributes test-link test-header-tagging openaction-1 test-fakespace-tagpdf test-header-footer disable-delayed-shipout test-beast-1 test-attribute-name test-attribute-dev test-alttext test-AFref
+
+
+  l3build check --show-saves spacecharoff
