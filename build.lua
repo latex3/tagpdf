007e3577a889820acf2d488a4466575ad754ec3c
Ulrike Fischer <fischer@troubleshooting-tex.de>
2024-09-11 23:26:57 +0200

    change note

--- build.lua
+++ build.lua
@@ -26,7 +26,7 @@ uploadconfig = {
   email    = mydata.email,
   update   = true ,
   topic=    "tagged-pdf",
-  note     = [[The description and summary has been changed! Uploaded automatically by l3build...]],
+  note     = [[Uploaded automatically by l3build...]],
   description=[[The package contains the core code for tagging and accessibility used by the LaTeX kernel in the Tagged PDF project. See https://github.com/latex3/tagging-project for more information.]],
   announcement_file="ctan.ann"
 }
