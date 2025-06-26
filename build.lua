112daa287ca965a8c08d7ee9a9a169d9d0b5b640
Ulrike Fischer <fischer@troubleshooting-tex.de>
2025-06-26 11:20:57 +0200

    fix announcement text

--- build.lua
+++ build.lua
@@ -26,8 +26,7 @@ uploadconfig = {
   email    = mydata.email,
   update   = true ,
   topic=    "tagged-pdf",
-  note     = [[Attention! There is a new ltx file that should be installed into tex/latex/tagpdf!
-               Uploaded automatically by l3build...]],
+  note     = [[Uploaded automatically by l3build...]],
   description=[[The package contains the core code for tagging and accessibility used by the LaTeX kernel in the Tagged PDF project. See https://github.com/latex3/tagging-project for more information.]],
   announcement_file="ctan.ann"
 }
